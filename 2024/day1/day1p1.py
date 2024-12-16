d565e62edf12607fc551836816b4cff42541ab9f
Alvaro <martinhuertaalvaro@gmail.com>
2024-12-16 21:56:35 +0100

    ajustes

--- 2024/day1/day1p1.py
+++ 2024/day1/day1p1.py
@@ -5,10 +5,8 @@ r = []
 with open('day1.txt', 'r', encoding='utf-8') as archivo:
     for linea in archivo:
         numeros = linea.strip().split()
-        
-        if len(numeros) == 2:
-            l.append(int(numeros[0]))
-            r.append(int(numeros[1]))
+        l.append(int(numeros[0]))
+        r.append(int(numeros[1]))
 l.sort()
 r.sort()
 
--- 2024/day1/day1p2.py
+++ 2024/day1/day1p2.py
@@ -7,10 +7,8 @@ r = []
 with open('day1.txt', 'r', encoding='utf-8') as archivo:
     for linea in archivo:
         numeros = linea.strip().split()
-        
-        if len(numeros) == 2:
-            l.append(int(numeros[0]))
-            r.append(int(numeros[1]))
+        l.append(int(numeros[0]))
+        r.append(int(numeros[1]))
 
 conteo_l = Counter(l)
 conteo_r = Counter(r)
